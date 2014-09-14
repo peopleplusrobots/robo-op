0dd6a04623152bb1afde8b3558bfb1b0d67e545f
madlab <madeline.gannon@gmail.com>
2014-09-13 20:13:48 -0400

    added OSC communication

--- src/com/OSC.java
+++ src/com/OSC.java
@@ -1,14 +1,99 @@
 package com;
 
+import gui.GUI;
+import oscP5.*;
+import netP5.*;
+
+
 public class OSC {
 
-	public OSC(){
-		
-	}
+	private GUI p5;
+	
+	private OscP5 oscP5;
+	private NetAddress addr;
 
-	public String start() {
-		
-		return "dummy function";
+	private static final int PORT  = 12001;
+	private static final String IP_DEFAULT 		 = "127.0.0.1"; 
+	// fill in custom IP addresses to other OSC compatible softwares
+	private static final String IP_GRASSHOPPER3D = "172.16.214.128";
+	private static final String IP_VVVV			 = "XXX.XX.XXX.XXX";
+	private static final String IP_MAXMSP 		 = "XXX.XX.XXX.XXX";
+	
+	public OSC(GUI p5){	
+		this.p5 = p5;
 	}
 	
+
+	public String start() {
+		oscP5 = new OscP5(this,PORT);
+		addr = new NetAddress(IP_DEFAULT,PORT);
+		
+		/*
+		 * You can write custom functions to handle incoming messages.
+		 */
+		oscP5.plug(this, "testCOM", "/test");
+		
+		return "OSC is setup";
+	}
+	
+	
+	
+	/**
+	 * Example for how to create a listener for a particular OSC message.
+	 */
+	public void testCOM(String s0, String s1, String s2, String s3, String s4){
+		String msg = s0+" "+s1+" "+s2+" "+s3+" "+s4;
+		System.out.println("testCOM: "+msg);
+		
+		// pass the message to the GUI for visualizing or dispatching
+		p5.setTest(msg); 
+	}
+	
+	/**
+	 * Example for how to send a message through OSC
+	 */
+	public void sendDummyMessage(){
+		
+		OscMessage msg = new OscMessage("/test");
+		msg.add("this");
+		msg.add("is");
+		msg.add("just");
+		msg.add("a");
+		msg.add(Math.random()+"");
+		
+		oscP5.send(msg,addr);		
+	}
+	
+	/**
+	 * Example for how to send a message through OSC
+	 */
+	public void sendRandomPt(){
+		OscMessage msg = new OscMessage("/point");
+		msg.add(Math.random()+"");
+		msg.add(Math.random()+"");
+		msg.add(Math.random()+"");
+		
+		oscP5.send(msg,addr);	
+	}
+	
+	/**
+	 * Listens for an OSC message 
+	 * @param incoming - message being sent here
+	 */
+	public void oscEvent(OscMessage incoming) {
+		
+		// if we haven't created a specific listener for a kind of message
+		if (!incoming.isPlugged()){
+			System.out.print("### received an osc message.");
+			System.out.print(" addrpattern: "+incoming.addrPattern());
+			System.out.println(" typetag: "+incoming.typetag());
+			
+			System.out.println("values: ");
+			for (int i=0; i<incoming.addrPattern().length(); i++){
+				if (incoming.get(i) != null)
+					System.out.println(incoming.get(i));
+			}
+			System.out.println();
+		}	
+	}
 }
--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -7,6 +7,7 @@ import com.OSC;
 import com.Robot;
 
 import processing.core.PApplet;
+import processing.core.PFont;
 
 /**
  * This is our Processing sketch. 	<br/>
@@ -19,10 +20,17 @@ public class GUI extends PApplet {
 
 	private Frame parent;
 	
+	PFont font = createFont("Monospaced-38.vlw",38);
+	private String oscMsg = "";
+	
 	// External Communication Streams
 	private Robot robot;
 	private Arduino arduino;
 	private OSC osc;
+	
+	private boolean robotMode = false;
+	private boolean arduinoMode = false;
+	private boolean oscMode = true;
 		
 	/*
 	 *  Add additional variables below:
@@ -49,14 +57,24 @@ public class GUI extends PApplet {
 	public void setup() {
 		background(100);
 		size(parent.getWidth(),parent.getHeight());
+		
+		textAlign(CENTER,CENTER);
+		textFont(font, 18);
 			
 		// create a communication stream to the robot
 		println("Setting up robot's socket connection ... ");
 		this.robot = new Robot(this);
-		thread("startRobot");
+		this.osc = new OSC(this);
 		
+		if (robotMode)
+			thread("startRobot");
+		if (oscMode)
+			thread("startOSC");
 	}
 	
+	/*
+	 * Start each kind of communication on it's own thread.
+	 */
 	public String startRobot(){
 		return robot.connect();
 	}	
@@ -75,24 +93,23 @@ public class GUI extends PApplet {
 	public void draw() {
 		smooth();
 		background(counter);
-		
-		// Your code goes here:
 		counter+=1;
 		counter %=255;
+		
+		// testing OSC connection
+		if (oscMode){	
+			text("Message from OSC: "+oscMsg, width/2, height/2);
+		}
+		
+		
+		// Your code goes here:
+		
 
 	}
 	
 	/*
 	 * Add custom functions below
 	 */
-
-//	/**
-//	 * Example: Send a message to the robot when the mouse is clicked.
-//	 */
-//	public void mouseClicked(){
-//		
-//		
-//	}
 	
 	
 	/**
@@ -104,54 +121,64 @@ public class GUI extends PApplet {
 	
 	
 	/**
-	 * Send new target when mouse is clicked
+	 * Or send new target when mouse is clicked
 	 */
 	int testCounter = 0;
 	public void mouseClicked(){
 
-		println(testCounter);
+		if (oscMode){
+			osc.sendDummyMessage();
+			osc.sendRandomPt();
+		}
 		
-//		if (testCounter == 0)
-//			robot.sendDummyTarget();
-//		if (testCounter == 1)
-//			robot.moveTo(900, -300, 1500, -80, 60, -100);
-//		else if (testCounter == 2)
-//			robot.moveOffset(5, 5, 10, 0, 0, 0);
-//		else if (testCounter == 3)
-//			robot.getSpeed();
-//		else if (testCounter == 4)
-//			robot.getZone();
-//		else if (testCounter == 5){
-//			println("trying to set speed: ");
-//			robot.setSpeed(10, 30, 0, 0);
-//			robot.getSpeed();
-//		}
-//		else if (testCounter == 6){
-//			println("trying to set zone: ");
-//			robot.setZone(Robot.z15);
-//			robot.getZone();
-//		}
-		if (testCounter == 1)
-			robot.setPosition(900,-280,1400);
-		if (testCounter == 2)
-			robot.setOrientation(-45,50,-45);
-		if (testCounter == 3)
-			robot.getConfiguration();
-		if (testCounter == 4)
-			robot.getExternalAxes();
-		if (testCounter == 5)
-			robot.getRobTarget();
-		if (testCounter == 6)
-			robot.setConfiguration("[-1,0,-1,0]");
-		if (testCounter == 7)
-			robot.setExternalAxes("[100,9E+09,9E+09,9E+09,9E+09,25]");
-		else if (testCounter == 8)
-			robot.quit();
+		if (robotMode){
+			
+			// testing different functions for robot com
+			
+//			if (testCounter == 1)
+//				robot.moveTo(900, -300, 1500, -80, 60, -100);
+//			else if (testCounter == 2)
+//				robot.moveOffset(5, 5, 10, 0, 0, 0);
+//			else if (testCounter == 3)
+//				robot.getSpeed();
+//			else if (testCounter == 4)
+//				robot.getZone();
+//			else if (testCounter == 5){
+//				println("trying to set speed: ");
+//				robot.setSpeed(10, 30, 0, 0);
+//				robot.getSpeed();
+//			}
+//			else if (testCounter == 6){
+//				println("trying to set zone: ");
+//				robot.setZone(Robot.z15);
+//				robot.getZone();
+//			}
+			if (testCounter == 1)
+				robot.setPosition(900,-280,1400);
+			if (testCounter == 2)
+				robot.setOrientation(-45,50,-45);
+			if (testCounter == 3)
+				robot.getConfiguration();
+			if (testCounter == 4)
+				robot.getExternalAxes();
+			if (testCounter == 5)
+				robot.getRobTarget();
+			if (testCounter == 6)
+				robot.setConfiguration("[-1,0,-1,0]");
+			if (testCounter == 7)
+				robot.setExternalAxes("[100,9E+09,9E+09,9E+09,9E+09,25]");
+			else if (testCounter == 8)
+				robot.quit();
+			
+		}
 		
 		testCounter++;
 		background(255,0,0);
 	}
 
+	public void setTest(String msg){
+		oscMsg = msg;
+	}
 	
 	@Override
 	public void exit(){
